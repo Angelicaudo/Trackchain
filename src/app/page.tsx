375802bfa196d8388ea78a322d818ba8ed5562e5
Angelicabassey <angelicabassey44@gmail.com>
2024-10-03 11:19:18 +0100

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,100 @@
 import Image from "next/image";
+import { IoIosSearch } from "react-icons/io";
+
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className=" bg-black">
+      <section className="bg-heroImg bg-cover bg-center h-screen bg-repeat">
+        <nav className="flex justify-evenly sm:justify-between items-center	 px-0 sm:px-24 py-4">
+          <div>
+            <Image src="/logooo.png" alt={"img"} height={"160"} width={"160"} />
+          </div>
+          <div>
+            <button className=" text-white border rounded-2xl p-2 border-red-600">CONNECT WALLET </button>
+          </div>
+        </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* hero */}
+        <section>
+          <div className="flex flex-col justify-center items-center gap-8 top-28 relative">
+            <div className="flex flex-col gap-5">
+              <div>
+                <Image src="/herobg.png" alt={"img"} height={"400"} width={"400"} />
+              </div>
+
+              <div>
+                <p className="text-white text-center tracking-widest">Effortlessly record and track the complete ownership <br /> history of your items.</p>
+              </div>
+            </div>
+
+            <div>
+
+              <div className="flex items-center  border-2 border-red-600 rounded-xl overflow-hidden w-full max-w-md bg-white text-black">
+                <IoIosSearch className="h-8 w-8 text-stone-950 mx-3 text-8xl-" />
+                <input
+                  type="text"
+                  placeholder="Search using item id"
+                  className="w-full sm:w-[300px]  py-2 px-3 focus:outline-none placeholder:text-black"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* verify item */}
+        <section className="top-60 relative">
+
+          <div className="text-white flex justify-center flex-col items-center gap-8">
+            <div className="border border-red-600 rounded-2xl">
+              <p className="px-4 py-2 ">Newly Verified Item</p>
+            </div>
+
+            <div>
+              <p className="text-center">Ownership is verified before an item is added to the <br /> blockchain</p>
+            </div>
+            {/* box id */}
+            <div className="grid grid-cols-2  sm:grid-cols-5 gap-4">
+              <div className="bg-zinc-900 hover:bg-white hover:text-black px-8 py-2 rounded-2xl">
+                <img src="boxImg.png" alt="" height={"80"} width={"80"} />
+                <p>arbiter34..</p>
+              </div>
+
+              <div className="bg-zinc-900 hover:bg-white hover:text-black px-8 py-2 rounded-2xl">
+                <img src="boxImg.png" alt="" height={"80"} width={"80"} />
+                <p>arbiter34..</p>
+              </div>
+
+              <div className="bg-zinc-900 hover:bg-white hover:text-black px-8 py-2 rounded-2xl">
+                <img src="boxImg.png" alt="" height={"80"} width={"80"} />
+                <p>arbiter34..</p>
+              </div>
+
+              <div className="bg-zinc-900 hover:bg-white hover:text-black px-8 py-2 rounded-2xl">
+                <img src="boxImg.png" alt="" height={"80"} width={"80"} />
+                <p>arbiter34..</p>
+              </div>
+
+              <div className="bg-zinc-900 hover:bg-white hover:text-black px-8 py-2 rounded-2xl">
+                <img src="boxImg.png" alt="" height={"80"} width={"80"} />
+                <p>arbiter34..</p>
+              </div>
+            </div>
+          </div>
+          {/*  */}
+
+        </section>
+
+
+      </section>
+
+
+
+
+
+
+
     </div>
   );
 }
